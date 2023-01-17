c4c7b61b77ffb14cb6040c90bb370fea6aee293b
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-17 12:02:25 -0500

    updated tables styling on dashboard

--- front-end/src/tables/ListTables.js
+++ front-end/src/tables/ListTables.js
@@ -1,33 +1,47 @@
 
-
 function ListTables({ tables, handleFinish }) {
     const tableList = tables.map((table) => {
         return (
-            <div key={table.table_id} className="text-center border border-primary m-2 p-2">
-                <div>
-                    <h3>Table Name: {table.table_name}</h3>
-                    <h3 data-table-id-status={table.table_id}>
-                        Status:
-                        {table.reservation_id ? "Occupied" : "Free"}
-                    </h3>
-                    <h3>Capacity: {table.capacity}</h3>
-                </div>
+        <div className="table-card d-inline-flex flex-wrap">
+            <div key={table.table_id}>
+                <table className="table table-striped table-light">
+                    <tbody>
+                        <tr>
+                            <td>Table Name: </td>
+                            <td>{table.table_name}</td>
+                        </tr>
+                        <tr>
+                            <td data-table-id-status={table.table_id}>Status: </td>
+                            <td>{table.reservation_id ? "Occupied" : "Free"}</td>
+                        </tr>
+                        <tr>
+                            <td>Capacity: </td>
+                            <td>{table.capacity}</td>
+                        </tr>
+                    </tbody>
+                </table>
 
-                {table.reservation_id && (
+                {table.reservation_id ? (
+        <div className="text-center">
           <button
             data-table-id-finish={table.table_id}
             value={table.reservation_id}
             id={table.table_id}
-            className="btn btn-primary"
+            className="btn btn-primary mr-3"
             onClick={handleFinish}
           >
             Finish
           </button>
+        </div>
+        ) : (
+            <div className="cancelled-finish-padding"> </div>
         )}
 
             </div>
+        </div>
         );
     });
+    
 
     return (
         <div>
