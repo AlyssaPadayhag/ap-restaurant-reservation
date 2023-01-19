3e59073c482bb170af818d1725200981acdb289a
Alyssa Padayhag <alyssapadayhag@gmail.com>
2023-01-19 11:29:17 -0500

    updated tables styling

--- front-end/src/tables/ListTables.js
+++ front-end/src/tables/ListTables.js
@@ -4,30 +4,30 @@ function ListTables({ tables, handleFinish }) {
         return (
         <div className="table-card d-inline-flex flex-wrap">
             <div key={table.table_id}>
-                <table className="table table-striped table-light">
+                <table className="table table-striped table-light p-0 m-0">
                     <tbody>
                         <tr>
                             <td>Table Name: </td>
                             <td>{table.table_name}</td>
                         </tr>
                         <tr>
-                            <td data-table-id-status={table.table_id}>Status: </td>
-                            <td>{table.reservation_id ? "Occupied" : "Free"}</td>
-                        </tr>
-                        <tr>
                             <td>Capacity: </td>
                             <td>{table.capacity}</td>
+                        </tr>
+                        <tr>
+                            <td data-table-id-status={table.table_id}>Status: </td>
+                            {table.reservation_id ? <td id="occupied">Occupied</td> : <td id="free">Free</td>}
                         </tr>
                     </tbody>
                 </table>
 
                 {table.reservation_id ? (
-        <div className="text-center">
+        <div className="text-center table-button-border">
           <button
             data-table-id-finish={table.table_id}
             value={table.reservation_id}
             id={table.table_id}
-            className="btn btn-primary mr-3"
+            className="btn btn-success mr-3"
             onClick={handleFinish}
           >
             Finish
